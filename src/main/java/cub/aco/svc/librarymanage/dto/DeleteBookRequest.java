34fdc01aee906648adc93af8bda3f8541ff07252
chien-ping <pingchien.biz@gmail.com>
2025-06-18 05:55:51 +0800

    feat: 升級Spring Boot至3.2.12並從Springfox遷移至OpenAPI

--- src/main/java/cub/aco/svc/librarymanage/dto/DeleteBookRequest.java
+++ src/main/java/cub/aco/svc/librarymanage/dto/DeleteBookRequest.java
@@ -3,9 +3,9 @@ package cub.aco.svc.librarymanage.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class DeleteBookRequest {
